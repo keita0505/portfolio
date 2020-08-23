3c7b5b9648cc000dfcb7ac3d6bfc6ec5a96f25a0
Keita0505 <kanakei0505@yahoo.co.jp>
2020-08-23 18:27:49 +0900

    fin_commit
    fin_commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 // ヘッダー
 $(function(){
   $(".portfolio__titles__title").click(function(){
-    $(this).toggleClass("active")
+    
     if($(this).hasClass('active')){
+      $('body,html').stop().animate({scrollTop:0}, 1000,"swing")
+      $(this).removeClass("active")
+      setTimeout(function(){
+        $(".portfolio__titles__navigations").fadeOut(2000);
+        $("section").fadeOut()
+       },1000);
+      
+    } else {
+      $(this).addClass("active")
       $(".portfolio__titles__navigations").fadeIn(0);
       $("section").fadeIn()
       $(".js-spanize").lettering();
-    } else {
-      $(".portfolio__titles__navigations").fadeOut(2000);
-      $("section").fadeOut()
     }
   })
 })
@@ -30,14 +36,53 @@ $(function(){
     $('body,html').stop().animate({scrollTop:position}, 500,"swing");
   }
 })
-// セクション01
-$(function(){
-  $(".about-book").click(function(){
-    $(this).toggleClass("active")
-    if($(this).hasClass('active')){
-      $(".fadein").fadeIn(3000)
+
+$(function() {
+  var $win = $(window),
+      $nav = $('.portfolio__titles'),
+      navPos = $nav.offset().top - 15,
+      fixedClass = 'is-fixed';
+
+  $win.on('load scroll', function() {
+    var value = $(this).scrollTop();
+    if ( value > navPos ) {
+      $nav.addClass(fixedClass);
     } else {
-      $(".fadein").fadeOut()
+      $nav.removeClass(fixedClass);
     }
+  });
+});
+
+$(function(){
+  $(".portfolio-site").click(function(){
+    $(".modal").fadeIn(500)
   })
 })
+
+$(function(){
+  $(".modal").click(function(){
+    $(".modal").fadeOut(500)
+  })
+})
+
+$(function(){
+  $(".slide__images__image").mouseover(function(e){
+    var src = $(this).children('img').attr('src')
+    $(".slide__main-image").empty()
+    $(".slide__main-image").append('<img src=""  style="display: none;">')
+    $(".slide__main-image").children('img').attr('src',src)
+    $(".slide__main-image").children('img').fadeIn(500)
+  })
+})
+
+// セクション01
+// $(function(){
+//   $(".about-book").click(function(){
+//     $(this).toggleClass("active")
+//     if($(this).hasClass('active')){
+//       $(".fadein").fadeIn(3000)
+//     } else {
+//       $(".fadein").fadeOut()
+//     }
+//   })
+// })
